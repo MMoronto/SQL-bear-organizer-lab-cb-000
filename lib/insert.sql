4f3628d27770e9d73c992f9e07ae3ac424106634
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-12-04 19:14:42 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender) VALUES ( 'Mr. Chocolate', 5, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Rowdy', 3, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Tabitha', 5, 'F');
+INSERT INTO bears (name, age, gender) VALUES ( 'Seargent Brown', 7, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Melissa', 6, 'F');
+INSERT INTO bears (name, age, gender) VALUES ( 'Grinch', 9, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Wendy', 11, 'F');
+INSERT INTO bears (name, age, gender) VALUES ( NULL, NULL, 'M');
