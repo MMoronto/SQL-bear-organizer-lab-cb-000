38ffd76d0f19edbb0e3d18475b9c4bc7701fff58
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-12-04 18:22:46 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,8 @@
-INSERT INTO bears (name, age, gender) 
-VALUES ( 'Mr. Chocolate', 5, 'M');
-VALUES ( 'Rowdy', 3, 'M');
-VALUES ( 'Tabitha', 5, 'F');
-VALUES ( 'Seargent Brown', 7, 'M');
-VALUES ( 'Melissa', 6, 'F');
-VALUES ( 'Grinch', 9, 'M');
-VALUES ( 'Wendy', 11, 'F');
-VALUES ( NULL, NULL, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Mr. Chocolate', 5, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Rowdy', 3, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Tabitha', 5, 'F');
+INSERT INTO bears (name, age, gender) VALUES ( 'Seargent Brown', 7, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Melissa', 6, 'F');
+INSERT INTO bears (name, age, gender) VALUES ( 'Grinch', 9, 'M');
+INSERT INTO bears (name, age, gender) VALUES ( 'Wendy', 11, 'F');
+INSERT INTO bears (name, age, gender) VALUES ( NULL, NULL, 'M');
