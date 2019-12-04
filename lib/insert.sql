3075cdf9445f8e3e06af0cf65d330ebc0efa1589
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-12-04 18:18:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,5 @@
 INSERT INTO bears (name, age, gender) 
 VALUES ( 'Mr. Chocolate', 5, 'M');
 VALUES ( 'Mr. Chocolate', 5, 'M');
+VALUES ( 'Mr. Chocolate', 5, 'M');
+VALUES ( 'Mr. Chocolate', 5, 'M');
