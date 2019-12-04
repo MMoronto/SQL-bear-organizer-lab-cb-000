11622e6cba3bdf5493afa1df8c19f92f6fdc4acd
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-12-04 18:21:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,9 @@
 INSERT INTO bears (name, age, gender) 
 VALUES ( 'Mr. Chocolate', 5, 'M');
-VALUES ( 'Mr. Chocolate', 5, 'M');
-VALUES ( 'Mr. Chocolate', 5, 'M');
-VALUES ( 'Mr. Chocolate', 5, 'M');
+VALUES ( 'Rowdy', 3, 'M');
+VALUES ( 'Tabitha', 5, 'F');
+VALUES ( 'Seargent Brown', 7, 'M');
+VALUES ( 'Melissa', 6, 'F');
+VALUES ( 'Grinch', 9, 'M');
+VALUES ( 'Wendy', 11, 'F');
+VALUES ( NULL, NULL, 'M');
